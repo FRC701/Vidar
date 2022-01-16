64a730a5df63a7896303c953ed2167e93b59cd42
Kaitlyn Pereira <Kaitlynp@vandenrobotics.com>
2022-01-16 10:34:04 -0800

    Made a shooter subsystem

---
+++ Vidar/src/main/include/subsystems/Shooter.h
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc2/command/SubsystemBase.h>
+#include <ctre/Phoenix.h>
+
+class Shooter : public frc2::SubsystemBase {
+ public:
+  Shooter(WPI_TalonFX& FlywheelMotor, WPI_TalonFX& HoodMotor);
+
+  /**
+   * Will be called periodically whenever the CommandScheduler runs.
+   */
+  void Periodic() override;
+  double SpinFlywheel(double speed);
+  void MoveHood(double speed);
+
+
+ private:
+ WPI_TalonFX& FlywheelMotor;
+ WPI_TalonFX& HoodMotor;
+  // Components (e.g. motor controllers and sensors) should generally be
+  // declared private and exposed only through public methods.
+};
