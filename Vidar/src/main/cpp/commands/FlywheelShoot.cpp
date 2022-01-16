dbbf285ec60b4c4805cfc1d03b5738bb24ebb5c4
Kaitlyn Pereira <Kaitlynp@vandenrobotics.com>
2022-01-16 10:34:57 -0800

    Made two commands
    for flywheel and hood of shooter

---
+++ Vidar/src/main/cpp/commands/FlywheelShoot.cpp
@@ -0,0 +1,32 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/FlywheelShoot.h"
+
+FlywheelShoot::FlywheelShoot(Shooter& mShooter, double motorspeed) 
+: mShooter(mShooter)
+, motorspeed(motorspeed)
+{
+  // Use addRequirements() here to declare subsystem dependencies.
+}
+
+// Called when the command is initially scheduled.
+void FlywheelShoot::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+void FlywheelShoot::Execute() 
+{
+mShooter.SpinFlywheel(motorspeed);
+}
+
+// Called once the command ends or is interrupted.
+void FlywheelShoot::End(bool interrupted) 
+{
+  mShooter.SpinFlywheel(0.0);
+}
+
+// Returns true when the command should end.
+bool FlywheelShoot::IsFinished() {
+  return false;
+}
---
+++ Vidar/src/main/cpp/commands/HoodEngage.cpp
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/HoodEngage.h"
+
+HoodEngage::HoodEngage(Shooter& mShooter, double hoodspeed) 
+: mShooter(mShooter)
+, hoodspeed(hoodspeed)
+
+{
+  // Use addRequirements() here to declare subsystem dependencies.
+}
+
+// Called when the command is initially scheduled.
+void HoodEngage::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+void HoodEngage::Execute()
+ {
+   mShooter.MoveHood(hoodspeed);
+ }
+
+// Called once the command ends or is interrupted.
+void HoodEngage::End(bool interrupted) 
+{
+  mShooter.MoveHood(0.0);
+}
+
+// Returns true when the command should end.
+bool HoodEngage::IsFinished() {
+  return false;
+}
