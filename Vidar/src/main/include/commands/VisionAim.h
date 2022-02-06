f33b7341c360545a93cb71ffc02daa6457722681
Duane Murphy <dmurphy@vandenrobotics.com>
2022-02-06 11:04:40 -0800

    Add VisionAim PID Command
    Just the template.

---
+++ Vidar/src/main/include/commands/VisionAim.h
@@ -0,0 +1,19 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc2/command/CommandHelper.h>
+#include <frc2/command/PIDCommand.h>
+
+// VisionAim is a command that uses PID to aim the chassis at the target
+// The input to the PID is vision information from the Limelight
+// The output is the chassis moving right and left
+class VisionAim
+    : public frc2::CommandHelper<frc2::PIDCommand, VisionAim> {
+ public:
+  VisionAim();
+
+  bool IsFinished() override;
+};
