3b8eb22417ec6281e50604a8a42d237396a5a81a
Elijah Capalla <elijahc@vandenrobotics.com>
2022-02-17 16:01:37 -0800

    Added Camera for Feeder
    :)

--- Vidar/src/main/cpp/Robot.cpp
+++ Vidar/src/main/cpp/Robot.cpp
@@ -7,8 +7,10 @@
 #include <frc/smartdashboard/SmartDashboard.h>
 #include <frc2/command/CommandScheduler.h>
 
-void Robot::RobotInit() {}
-
+void Robot::RobotInit() 
+{
+    frc::CameraServer::StartAutomaticCapture();
+}
 /**
  * This function is called every robot packet, no matter the mode. Use
  * this for items like diagnostics that you want to run during disabled,
