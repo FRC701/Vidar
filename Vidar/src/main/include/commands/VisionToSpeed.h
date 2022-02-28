6448eea3b1b4392f201722d7ad867932be463928
Duane Murphy <dmurphy@vandenrobotics.com>
2022-02-27 16:57:10 -0800

    Declare FindSpeed
    Refactoring a bit more. Now FindSpeed is declared but not defined. This causes a linker error. Still red for testing.
    
    The interface has been updated as we realize that a begin and end pointer are need. Passing an array is just a pointer to the beginning of the array, which is not enough information.

--- Vidar/src/main/include/commands/VisionToSpeed.h
+++ Vidar/src/main/include/commands/VisionToSpeed.h
@@ -9,5 +9,16 @@ struct VisionToSpeed
     double speedRPM;
 };
 
+// Return the linearly interpolated speed from the table identified from
+// begin to end looking for targetArea. 
+// 
+// The table must be sorted increasing by targetArea.
+//
+// Return: the RPM speed to use for the specified targetArea
+//   A return value of 0.0 indicates that some part of the data was unusable
+//   Examples:
+//   1. begin == end (the table is empty)
+//   2. targetArea is before or after the begin and end. Can only target between the max and the min
+double FindSpeed(VisionToSpeed* begin, VisionToSpeed* end, double targetArea);
 
 }   // end namespace vision_shoot
--- Vidar/src/test/cpp/TestVisionShoot.cpp
+++ Vidar/src/test/cpp/TestVisionShoot.cpp
@@ -2,9 +2,14 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+#include <algorithm>
+
 #include "../../test/cpp/TestVisionShoot.h"
 
 using vision_shoot::VisionToSpeed;
+using vision_shoot::FindSpeed;
+
+
 
 TEST_F(TestVisionShoot, EmptyDataFails) {
     VisionToSpeed emptyVisionToSpeeds {};
@@ -16,7 +21,7 @@ TEST_F(TestVisionShoot, EmptyDataFails) {
     // In this case, we'll use a special return value. 
     // A return vale of 0 for a speed will indicate that this data didn't return
     // anything useable.
-    double speed = FindSpeed(emptyVisionToSpeeds, kTestTargetArea);
+    double speed = FindSpeed(&emptyVisionToSpeeds, &emptyVisionToSpeeds, kTestTargetArea);
 
     EXPECT_EQ(speed, 0.0);
 }
