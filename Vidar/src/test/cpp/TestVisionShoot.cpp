fad2ae28489f170065d68e7eec78f5d1fd1b4ece
Duane Murphy <dmurphy@vandenrobotics.com>
2022-02-27 16:14:57 -0800

    Begin TestVisionShoot
    Start with a failure. This shows that the test is setup properly and we know the test will catch a problem.

---
+++ Vidar/src/test/cpp/TestVisionShoot.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "../../test/cpp/TestVisionShoot.h"
+
+TEST_F(TestVisionShoot, EmptyDataFails) {
+    EXPECT_TRUE(false);
+}
+
